7a5278720eedbba2bf3797cff7a04b52ff0da4b7
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2020-07-19 13:04:55 +0300

    Added some important changes

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -76,9 +76,11 @@ namespace Vidly.Controllers
         {
             //var customers = GetCustomers();
 
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
-            return View(customers);
+            //return View(customers);
+
+            return View();
         }
 
         public ActionResult Details(int id)
